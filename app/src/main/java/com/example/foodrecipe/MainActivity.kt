a2e9f9cc5c9b8c1401d2f80dfcd8c229dc4c18eb
unknown <sunilthapa5898@gmail.com>
2021-03-11 17:57:23 +1100

    recipe design

--- app/src/main/java/com/example/foodrecipe/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipe/MainActivity.kt
@@ -5,15 +5,21 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController;
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         navController = findNavController(R.id.navHostFragment)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.recipesFragment,
@@ -21,9 +27,12 @@ class MainActivity : AppCompatActivity() {
                 R.id.foodJokeFragment
             )
         )
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
-       // bottom
+    }
 
-        //bottomNavigationView.setupwithNavController(navController)
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()|| super.onSupportNavigateUp()
     }
 }
